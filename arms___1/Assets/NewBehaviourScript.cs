65678b1bc336ff352e3a0b89e56702457068174c
Student <student@mirea.ru>
2019-11-22 10:15:01 +0300

    FirstCommit

---
+++ arms___1/Assets/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Touch box in Input.touches)
+            if (box.phase == TouchPhase.Moved)
+            {
+                transform.Rotate(new Vector3(0, -box.deltaPosition.x, 0));
+
+
+            }
+
+    }
+}
---
+++ arms___1/Assets/NewBehaviourScript1.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    int l;
+    public GameObject obj;
+    public bool start;
+    public float timer;
+    private void Start()
+    {
+        start = true;
+    }
+    void Update()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (l == 2)
+                    {
+                        l = 0;
+                    }
+                    l += 1;
+
+                }
+            }
+        }
+        if (start == false && l == 2)
+        {
+            timer += Time.deltaTime;
+            if (timer > 0 && timer < 1)
+            {
+                gameObject.transform.position += (new Vector3(0, Time.deltaTime * 0.1f, 0));
+            }
+            if (timer > 1) // сброс всех параметров в начальное положение при завершении движения
+            {
+                timer = 0;
+                start = true;
+                obj.SetActive(false);
+            }
+        }
+        if (start == true && l == 1)
+        {
+            obj.SetActive(true);
+            timer += Time.deltaTime;
+            if (timer > 0 && timer < 1) // когда таймер находится между 0 и 1
+            {
+                gameObject.transform.position += new Vector3(0, -Time.deltaTime * 0.1f, 0); //Изменение положения объекта. Чтобы он поехал в другую сторону нужно только поставить минус где надо
+            }
+            if (timer > 1) // сброс всех параметров в начальное положение при завершении движения
+            {
+                timer = 0;
+                start = false;
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+
+
+    }
+}
