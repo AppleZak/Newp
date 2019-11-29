c2c4eefb31f2a0151494926502b41dcc41db13f0
Student <student@mirea.ru>
2019-11-29 11:50:46 +0300

    work

---
+++ arms___1/Assets/Audio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    [SerializeField] AudioSource audio;
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        audio.Play();
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown(buttonName:"Jump"))
+        {
+            audio.Play();
+        }
+    }
+}
